f67dea1ca8afad0375c9ff4efbced01504b50abe
Keith Irwin <irwinke@wssu.edu>
2017-04-19 09:39:23 -0400

    Bugfixes from SVN.  I'm not sure what in particular.

--- Instructors/views/instructorCourseHomeView.py
+++ Instructors/views/instructorCourseHomeView.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from Instructors.models import Courses, Challenges
 from Instructors.models import Skills, CoursesSkills 
 from Badges.models import CourseConfigParams
-from Students.models import StudentBadges,StudentChallenges,Student,StudentCourseSkills, StudentRegisteredCourses
+from Students.models import StudentBadges,StudentChallenges, StudentCourseSkills, StudentRegisteredCourses
 from Instructors.views.announcementListView import createContextForAnnouncementList
 from Instructors.views.upcommingChallengesListView import createContextForUpcommingChallengesList
 from _datetime import datetime
@@ -18,6 +18,125 @@ import inspect
 def lineno():
     """Returns the current line number in our program."""
     return inspect.currentframe().f_back.f_lineno
+
+def studentXP(studentId, courseId, courseChallenges):
+    
+    xp = 0
+    print(studentId)
+    print(courseId)
+    print(courseChallenges)
+    for challenge in courseChallenges:
+        sc = StudentChallenges.objects.filter(studentID=studentId, courseID=courseId,challengeID=challenge)
+        print(sc)
+        gradeID  = []
+                            
+        for c in sc:
+            gradeID.append(int(c.testScore)) 
+            print(c.testScore)                                
+        if(gradeID):
+            xp = xp + max(gradeID)      # max grade for this challenge
+
+    print(str(xp))
+    return xp
+
+def courseLeaderboard(currentCourse, context_dict):
+    
+    print("I am here!!")
+    if currentCourse:
+        ccparamsList = CourseConfigParams.objects.filter(courseID=currentCourse)
+            
+        if len(ccparamsList) > 0:
+            ccparams = ccparamsList[0] 
+            context_dict["badgesUsed"]=ccparams.badgesUsed
+            context_dict["leaderboardUsed"]=ccparams.leaderboardUsed
+            context_dict["classSkillsDisplayed"]=ccparams.classSkillsDisplayed
+            context_dict["numStudentsDisplayed"]=ccparams.numStudentsDisplayed
+            context_dict["numStudentBestSkillsDisplayed"] = ccparams.numStudentBestSkillsDisplayed
+            context_dict["numBadgesDisplayed"]=ccparams.numBadgesDisplayed
+            
+        badgeId = [] 
+        studentBadgeID=[]
+        studentID=[]
+        badgeID=[]
+        badgeName=[]
+        badgeImage = []
+        avatarImage =[]
+        N = 7
+        
+        date_N_days_ago = datetime.now() - timedelta(days=N)
+
+        #Displaying the list of challenges from database
+        badges = StudentBadges.objects.all().order_by('-timestamp')
+       
+        for badge in badges:
+            studentBadgeID.append(badge.studentBadgeID)
+            studentID.append(badge.studentID)
+            badgeID.append(badge.badgeID)
+            badgeName.append(badge.badgeID.badgeName)
+            badgeImage.append(badge.badgeID.badgeImage)
+            st_crs = StudentRegisteredCourses.objects.get(studentID=badge.studentID,courseID=currentCourse)                
+            avatarImage.append(st_crs.avatarImage)
+                          
+        # The range part is the index numbers.
+        context_dict['badgesInfo'] = zip(range(1,ccparams.numBadgesDisplayed+1),studentBadgeID,studentID,badgeID,badgeImage,avatarImage)
+
+        students = []
+        st_crs = StudentRegisteredCourses.objects.filter(courseID=currentCourse)                                           
+        for st_c in st_crs:
+            students.append(st_c.studentID)     # all students in the course
+                  
+        context_dict['skills'] = []
+        cskills = CoursesSkills.objects.filter(courseID=currentCourse)
+        for sk in cskills:
+            skill = Skills.objects.get(skillID=sk.skillID.skillID)
+
+            usersInfo=[] 
+                                             
+            for u in students:
+                skillRecords = StudentCourseSkills.objects.filter(studentChallengeQuestionID__studentChallengeID__studentID=u,skillID = skill)
+                skillPoints =0 ;
+                                                     
+                for sRecord in skillRecords:
+                    skillPoints += sRecord.skillPoints
+                if skillPoints > 0:
+                    uSkillInfo = {'user':u.user,'skillPoints':skillPoints,'avatarImage':st_c.avatarImage}
+                    print("userSkillLst",lineno(),uSkillInfo)
+                    #Sort and Splice here
+                    usersInfo.append(uSkillInfo) 
+            skillInfo = {'skillName':skill.skillName,'usersInfo':usersInfo[0:ccparams.numStudentsDisplayed]}
+            print("skillInfo",lineno(),skillInfo)
+            context_dict['skills'].append(skillInfo)
+      
+        # XP Points
+    
+        # get the challenges for this course
+        courseChallenges = Challenges.objects.filter(courseID=currentCourse, isGraded=True, isVisible=True)
+
+        # dictionary studentAvatar - XP
+        studentXP_dict = {}
+        for s in students:
+            sXP = studentXP(s, currentCourse, courseChallenges)
+            st_crs = StudentRegisteredCourses.objects.get(studentID=s,courseID=currentCourse)
+            studentXP_dict[st_crs.avatarImage] = sXP 
+            
+        # sort the dictionary by its values; the result is a list of pairs (key, value)
+        xp_pairs = sorted(studentXP_dict.items(), key=lambda x: x[1], reverse=True)
+        xp_pairs = xp_pairs[:ccparams.numStudentsDisplayed]
+        
+        avatarImage = []
+        xpoints = []
+        for item in xp_pairs:
+            if item[1] > 0:         # don't append if 0 XP points
+                avatarImage.append(item[0])
+                xpoints.append(item[1])
+        
+        context_dict['user_range'] = zip(range(1,ccparams.numStudentsDisplayed+1),avatarImage, xpoints)                 
+                   
+    else:
+        context_dict['course_Name'] = 'Not Selected'
+        
+    return context_dict
+    
 
 def instructorCourseHome(request):
  
@@ -35,160 +154,6 @@ def instructorCourseHome(request):
         context_dict = createContextForUpcommingChallengesList(currentCourse, context_dict)
         context_dict['course_Name'] = currentCourse.courseName
         
-        ccparamsList = CourseConfigParams.objects.filter(courseID=currentCourse)
-            
-        if len(ccparamsList) > 0:
-            ccparams = ccparamsList[0] 
-            context_dict["badgesUsed"]=ccparams.badgesUsed
-            context_dict["leaderboardUsed"]=ccparams.leaderboardUsed
-            context_dict["classSkillsDisplayed"]=ccparams.classSkillsDisplayed
-            context_dict["numStudentsDisplayed"]=ccparams.numStudentsDisplayed
-            context_dict["numStudentBestSkillsDisplayed"] = ccparams.numStudentBestSkillsDisplayed
-            context_dict["numBadgesDisplayed"]=ccparams.numBadgesDisplayed
-            
-        #Leaderboard Badges
-#         StudentBadges(models.Model):
-#     studentBadgeID = models.AutoField(primary_key=True)
-#     studentID = models.ForeignKey(Student, verbose_name="the student", db_index=True)
-#     badgeID = models.ForeignKey(Badges, verbose_name="the badge", db_index=True)
-#     objectID = models.IntegerField(default=-1,verbose_name="index into the appropriate table") #ID of challenge,assignment,etc. associated with a badge
-#     timestamp = models.DateTimeField() ## Timestamp for badge assignment date
-        badgeId = [] 
-        studentBadgeID=[]
-        studentID=[]
-        badgeID=[]
-        badgeName=[]
-        badgeImage = []
-        avatarImage =[]
-        timestamp=[]
-        N = 7
-        
-
-        date_N_days_ago = datetime.now() - timedelta(days=N)
-
-        #print ( datetime.now())
-        #print ("date_N_days_Ago")
-        #print (date_N_days_ago)
-        
-        
-        #Displaying the list of challenges from database
-        badges = StudentBadges.objects.all().order_by('-timestamp')
-       
-        for badge in badges:
-            studentBadgeID.append(badge.studentBadgeID)
-            studentID.append(badge.studentID)
-            badgeID.append(badge.badgeID)
-            badgeName.append(badge.badgeID.badgeName)
-            badgeImage.append(badge.badgeID.badgeImage)
-            st_crs = StudentRegisteredCourses.objects.get(studentID=badge.studentID,courseID=currentCourse)                
-            avatarImage.append(st_crs.avatarImage)
-#                 timestamp.append(badge.timestamp)
-                          
-            # The range part is the index numbers.
-        context_dict['badgesInfo'] = zip(range(1,ccparams.numBadgesDisplayed+1),studentBadgeID,studentID,badgeID,badgeImage,avatarImage)
-        #print (badgeID)
-        #print(studentID)
-        #print (badgeName)
-#         print (badge.timestamp)  
-            ## Leaderboard Points
-            
-        ## Get the studentID for the course, then for each student in a loop get the total for all challenges for a student in that course
-#         studentGradedChallenges = []
-#         studentChallenges = StudentChallenges.objects.filter(studentID=studentID, courseID=currentCourse)
-#         for st_challenge in studentChallenges:
-#             if st_challenge.challengeID.isGraded:
-#                 studentGradedChallenges.append(st_challenge)
-#             for lp in studentGradedChallenges:
-#                      
-#                     studentChall_ID.append(lp.studentChallengeID) #pk
-#                     chall_ID.append(lp.challengeID.challengeID) 
-#                     total.append(lp.testTotal) 
-#                     avatarImage.append(lp.studentID.avatarImage)           
-#                 
-#         context_dict['leaderpoints_range'] = zip(range(1,len(studentGradedChallenges)+1),studentChall_ID,chall_ID,total,avatarImage)
-        # The range part is the index numbers.
-         #Leaderboard Skills       
-        ##################
-
-        context_dict['skills'] = []
-        courseID=[]
-        cskills = CoursesSkills.objects.filter(courseID=currentCourse)
-        for sk in cskills:
-
-            skill = Skills.objects.get(skillID=sk.skillID.skillID)
-
-            usersInfo=[]
-            # TODO: Narrow down to only students in the current course   
-            
-            st_crs = StudentRegisteredCourses.objects.filter(courseID=currentCourse)        
-                                   
-            for st_c in st_crs:
-                u = st_c.studentID 
-                skillRecords = StudentCourseSkills.objects.filter(studentChallengeQuestionID__studentChallengeID__studentID=u,skillID = skill)
-                skillPoints =0 ;
-                                                     
-                for sRecord in skillRecords:
-                     skillPoints += sRecord.skillPoints
-                if skillPoints > 0:
-                    uSkillInfo = {'user':u.user,'skillPoints':skillPoints,'avatarImage':st_c.avatarImage}
-                    print("userSkillLst",lineno(),uSkillInfo)
-                    #Sort and Splice here
-                    usersInfo.append(uSkillInfo) 
-            skillInfo = {'skillName':skill.skillName,'usersInfo':usersInfo[0:ccparams.numStudentsDisplayed]}
-            print("skillInfo",lineno(),skillInfo)
-            context_dict['skills'].append(skillInfo)
-
-## Do the first list into a loop and slice and get the k elements
-      
-    # Leaderboard based on XP Points
-    #Displaying the list of challenges from database
-        challenges = Challenges.objects.filter(courseID=currentCourse, isGraded=True,  isVisible=True)
-        num_challs = challenges.count()
-        allgrades = []
-        gradeTotal = []
-        first_Name= []
-        last_Name= []
-        chall_Name= []
-        
-
-        #Displaying the list of students from database
-        user = Student.objects.all()
-        avatarImage =[]
-        print ("[LBXP-Points]:",user)
-        num_users = user.count()
-        
-        for i in range(0, num_users):  
-            grade = []
-            gradeMax = []
-            number  = []
-            numberMax  = []
-            sc_user = []
-            sc_chall = []
-            
-            for j in range(0, num_challs):  
-                if StudentChallenges.objects.filter(studentID=user[i], courseID=currentCourse, challengeID = challenges[j]) :
-                    
-                    sChallenges = StudentChallenges.objects.filter(studentID=user[i], courseID=currentCourse, challengeID = challenges[j])
-                    gradeID  = []
-                    
-                    for sc in sChallenges:
-                        gradeID.append(int(sc.testScore))
-                    numberMax.append(max(gradeID))
-                    
-                else:
-                    numberMax.append(0)
-                number = numberMax
-            gradeTotal.append(("%0.2f" %sum(number)))
-               
-#         for u in user:                            #Why the avatars of all the students??????????
-#             avatarImage.append(u.avatarImage)
-        gradeTotal.sort(reverse=True)
-        print(gradeTotal) 
-#         context_dict['user_range'] = zip(range(1,user.count()+1),avatarImage, gradeTotal)
-        context_dict['user_range'] = zip(range(1,ccparams.numStudentBestSkillsDisplayed+1),avatarImage, gradeTotal)
-        
-            
-    else:
-        context_dict['course_Name'] = 'Not Selected'
+    context_dict = courseLeaderboard(currentCourse, context_dict)
         
     return render(request,'Instructors/InstructorCourseHome.html', context_dict)
--- Students/views/studentCourseHomeView.py
+++ Students/views/studentCourseHomeView.py
@@ -3,17 +3,14 @@ Created on Sep 14, 2016
 
 '''
 from django.shortcuts import render
-from Instructors.models import Courses,Skills,CoursesSkills, Challenges
-from Badges.models import CourseConfigParams
-from Students.models import StudentConfigParams,Student,StudentBadges,StudentChallenges,StudentCourseSkills,StudentRegisteredCourses
+from Instructors.models import Courses
+from Students.models import StudentConfigParams,Student,StudentRegisteredCourses
 from Instructors.views.announcementListView import createContextForAnnouncementList
+from Instructors.views.instructorCourseHomeView import courseLeaderboard
 from Instructors.views.upcommingChallengesListView import createContextForUpcommingChallengesList
-# from Instructors.views.preferencesView import createContextForUpcommingChallengesList
 
 from Badges.enums import Event
 from Badges.events import register_event
-from _datetime import datetime
-from datetime import timedelta
 
 
 def StudentCourseHome(request):
@@ -36,19 +33,10 @@ def StudentCourseHome(request):
         context_dict = createContextForUpcommingChallengesList(currentCourse, context_dict)
         context_dict['course_Name'] = currentCourse.courseName
         st_crs = StudentRegisteredCourses.objects.get(studentID=sID,courseID=currentCourse)
-        context_dict['avatar'] = st_crs.avatarImage          
-        
-        ccparamsList = CourseConfigParams.objects.filter(courseID=currentCourse)
-        if len(ccparamsList) >0:
-            cparams = ccparamsList[0]
-            context_dict["badgesUsed"]=cparams.badgesUsed
-            context_dict["levelingUsed"]=cparams.levelingUsed
-            context_dict["leaderboardUsed"]=cparams.leaderboardUsed
-            context_dict["classSkillsDisplayed"]=cparams.classSkillsDisplayed
-            context_dict["numStudentsDisplayed"]=cparams.numStudentsDisplayed
-            context_dict["numStudentBestSkillsDisplayed"]=cparams.numStudentBestSkillsDisplayed
-            context_dict["numBadgesDisplayed"]=cparams.numBadgesDisplayed
-            
+        context_dict['avatar'] = st_crs.avatarImage    
+                      
+        context_dict = courseLeaderboard(currentCourse, context_dict)
+           
         scparamsList = StudentConfigParams.objects.filter(courseID=currentCourse, studentID=sID)    
         if len(scparamsList) > 0:
             scparams = scparamsList[0]
@@ -56,120 +44,10 @@ def StudentCourseHome(request):
             context_dict["displayLeaderBoard"]=scparams.displayLeaderBoard
             context_dict["displayClassAverage"]=scparams.displayClassAverage
             context_dict["displayClassSkills"]=scparams.displayClassSkills
-        
-        
-        # Leaderboard based on XP Points
-        #Displaying the list of challenges from database
-        challenges = Challenges.objects.filter(courseID=currentCourse, isGraded=True,  isVisible=True)
-        num_challs = challenges.count()
-        allgrades = []
-        gradeTotal = []
-        first_Name=[]
-        last_Name=[]
-        chall_Name=[]
-        
-
-        #Displaying the list of students from database
-        user = Student.objects.filter()
-        avatarImage =[]
-        print (user)
-        num_users = user.count()
-        
-        for i in range(0, num_users):  
-            grade = []
-            gradeMax = []
-            number  = []
-            numberMax  = []
-            sc_user = []
-            sc_chall = []
-            
-            for j in range(0, num_challs):  
-                if StudentChallenges.objects.filter(studentID=user[i], courseID=currentCourse, challengeID = challenges[j]) :
-                    
-                    sChallenges = StudentChallenges.objects.filter(studentID=user[i], courseID=currentCourse, challengeID = challenges[j])
-                    gradeID  = []
-                    
-                    for sc in sChallenges:
-                        gradeID.append(int(sc.testScore))
-                    numberMax.append(max(gradeID))
-                    
-                else:
-                    numberMax.append(0)
-                number = numberMax
-            gradeTotal.append(("%0.2f" %sum(number)))
-                
-#         for u in user:                            #Why the avatars of all the students??????????
-#             avatarImage.append(u.avatarImage)
-        gradeTotal.sort(reverse=True)
-#         context_dict['user_range'] = zip(range(1,user.count()+1),avatarImage, gradeTotal)
-        context_dict['user_range'] = zip(range(1,cparams.numStudentBestSkillsDisplayed+1),avatarImage, gradeTotal)
-         
-         #Leaderboard Skills       
-        context_dict['skills'] = []
-        cskills = CoursesSkills.objects.filter(courseID=currentCourse)
-        for sk in cskills:
-
-            skill = Skills.objects.get(skillID=sk.skillID.skillID)
-
-            usersInfo=[]
-            # TODO: Narrow down to only students in the current course 
-            
-            st_crs = StudentRegisteredCourses.objects.filter(courseID=currentCourse)        
-                                   
-            #user=Student.objects.all()
-            for st_c in st_crs:
-                u = st_c.studentID 
-                skillRecords = StudentCourseSkills.objects.filter(studentChallengeQuestionID__studentChallengeID__studentID=u,skillID = skill)
-                skillPoints =0 ;
-                    
-                for sRecord in skillRecords:
-                     skillPoints += sRecord.skillPoints
-                if skillPoints > 0:
-#                     skill_Points.append(skillPoints)
-                    uSkillInfo = {'user':u.user,'skillPoints':skillPoints,'avatarImage':st_c.avatarImage} 
-                    print("userSkillLst",uSkillInfo)
-                    usersInfo.append(uSkillInfo)
-            skillInfo = {'skillName':skill.skillName,'usersInfo':usersInfo}
-#             context_dict['usersSkillsInfo'] = zip(range(1,len(usersSkillsInfo)),uSkillInfo)
-            context_dict['skills'].append(skillInfo)
-        # Add needed information
-     
-        studentBadgeID=[]
-        studentID=[]
-        badgeID=[]
-        badgeName=[]
-        badgeImage = []
-        avatarImage =[]
-        N = 7               
-
-        date_N_days_ago = datetime.now() - timedelta(days=N)
-
-        print ( datetime.now())
-        print ("date_N_days_Ago")
-        print (date_N_days_ago)                
-        #Displaying the list of badges from database
-        badges = StudentBadges.objects.all().order_by('-timestamp')
-        print(badges)
-        for badge in badges:
-                studentBadgeID.append(badge.studentBadgeID)
-                studentID.append(badge.studentID)
-                badgeID.append(badge.badgeID)
-                badgeName.append(badge.badgeID.badgeName)
-                badgeImage.append(badge.badgeID.badgeImage)
-                st_crs = StudentRegisteredCourses.objects.get(studentID=badge.studentID,courseID=currentCourse)                
-                avatarImage.append(st_crs.avatarImage)
-   
-        print (badgeID)
-        print(studentID)
-        print (badgeName)
-        # The range part is the index numbers.
-        context_dict['badgesInfo'] = zip(range(1,cparams.numBadgesDisplayed+1),studentBadgeID,studentID,badgeID,badgeImage,avatarImage)
-        print (badgeID)
-        print (studentID)
-        print (badgeName)
-        
+           
     #Trigger Student login event here so that it can be associated with a particular Course
     register_event(Event.userLogin, request, None, None)
     print("User Login event was registered for the student in the request")
     
-    return render(request,'Students/StudentCourseHome.html', context_dict)
+    return render(request,'Students/StudentCourseHome.html', context_dict)           
+        
