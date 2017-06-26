e74d72c6c42c94273306a8b4ddf1e864ad72e351
Keith Irwin <irwinke@wssu.edu>
2017-06-26 00:06:44 -0400

    Fixes to broken migrations.

--- Instructors/migrations/0050_auto_20170524_1543.py
+++ Instructors/migrations/0050_auto_20170524_1543.py
@@ -15,24 +15,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='UploadedFiles',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('uploadedFile', models.FileField(max_length=500, upload_to='D:\\workspace\\oneUpER\\media\\textfiles/xmlfiles')),
-                ('uploadedFileName', models.CharField(default='', max_length=200)),
-                ('uploaded_at', models.DateTimeField(auto_now_add=True)),
-                ('uploadedFileCreator', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='Creator')),
-            ],
-        ),
-        migrations.AlterField(
-            model_name='lualibrary',
-            name='libFile',
-            field=models.FileField(max_length=5000, upload_to='D:\\workspace\\oneUpER\\media\\lua/uploadedLuaLibs'),
-        ),
-        migrations.AlterField(
-            model_name='uploadedimages',
-            name='imageFile',
-            field=models.FileField(max_length=500, upload_to='D:\\workspace\\oneUpER\\media\\images/uploadedInstructorImages'),
-        ),
     ]
