9c0cccc24d460f4ad55333e0e515c4582a35fb61
root <root@oneup.wssu.edu>
2017-08-31 19:03:55 -0400

    Moving towards proper deployment settings.

--- oneUp/settings.py
+++ oneUp/settings.py
@@ -23,12 +23,13 @@ MEDIA_URL =  'media/' #This is for the html
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6l1(5i-qm34-eb!@un9gc%(g$o^=rgw8l++0!o9t6-^($qi6&k'
+with open('/var/www/wsgi-projects/oneUp/oneUp/secret.key') as f:
+    SECRET_KEY = f.read.strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
