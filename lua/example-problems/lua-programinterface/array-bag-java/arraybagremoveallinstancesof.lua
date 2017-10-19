5ab99ac6f12272933b8d8419f5ca93bbf53ee290
Darina Dicheva <dichevad@wssu.edu>
2017-10-19 16:52:50 -0400

    changed points for ArrayBag problem

--- lua/example-problems/lua-programinterface/array-bag-java/arraybagremoveallinstancesof.lua
+++ lua/example-problems/lua-programinterface/array-bag-java/arraybagremoveallinstancesof.lua
@@ -1,7 +1,7 @@
 tests = {
-   {name="test1",command="java ArrayBag test1", points=5},
-   {name="test2",command="java ArrayBag test1", points=5},
-   {name="test3",command="java ArrayBag test1", points=5}   
+   {name="test1",command="java ArrayBag test1", points=3},
+   {name="test2",command="java ArrayBag test2", points=3},
+   {name="test3",command="java ArrayBag test3", points=4}   
 }
    
-checker = programinterface.program_checker("/home/dichevad/workspace/oneUpNew/lua/example-problems/lua-programinterface/array-bag-java","ArrayBag.java","javac *.java",15,tests)
+checker = programinterface.program_checker("/home/dichevad/workspace/oneUpNew/lua/example-problems/lua-programinterface/array-bag-java","ArrayBag.java","javac *.java",10,tests)
