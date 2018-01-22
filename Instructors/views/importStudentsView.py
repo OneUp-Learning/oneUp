f1d6a0effa98932ae7e7fb390c38b5d5fbe6cb4e
Darina Dicheva <dichevad@wssu.edu>
2018-01-22 15:31:39 -0500

    changes for enabling Student Import

--- Instructors/views/importStudentsView.py
+++ Instructors/views/importStudentsView.py
@@ -7,63 +7,92 @@ Created on April 12, 2017
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 
-from Instructors.models import Courses
+from Instructors.views.utils import initialContextDict
+from Instructors.models import UploadedFiles
 from Instructors.constants import anonymous_avatar
-from Students.models import Student, StudentRegisteredCourses
+from Students.models import Student, StudentRegisteredCourses, StudentConfigParams
 from django.contrib.auth.decorators import login_required
 
-
-
-
+import logging
+logger = logging.getLogger(__name__)
 
 def process_file(filename):
     ls = []
     fp = open(filename)
     
     for line in fp:
-        line = line.replace('"', '')
-        names = line.split(',',3)
-        ls.append((names[1], names[0],  names[2]+'@rams.wssu.edu',names[2] ))
-    return ls[1:]
+        #line = line.replace('"', '')
+        names = line.split(',')
+        print(names)
+        ls.append((names[1], names[0],  names[2].lstrip(), names[3], names[4]))
+    print(ls)    
+    return ls
 
 @login_required
 def importStudents(request):
     
-    context_dict = { }
+    # IMPORTANT: We presume that since the instructor is getting the file from a university system, 
+    # there will be only two cases: 1/ the student is not in OneUp; 2/ The student is in OneUp but not in this course  
+              
+    context_dict, currentCourse = initialContextDict(request)
+    context_dict['usertype'] = 'Student'
+    context_dict['message'] = ''
+    ccparams = context_dict['ccparams']
+
+    if request.method == 'POST' and len(request.FILES) != 0:     
+        studentslistFile = request.FILES['studentslist']
+        studentslistFileName = studentslistFile.name 
+        print(studentslistFileName)
+          
+        upfile = UploadedFiles() 
+        upfile.uploadedFile = studentslistFile        
+        upfile.uploadedFileName = studentslistFileName
+        upfile.uploadedFileCreator = request.user
+        upfile.save()
     
-    # check if course was selected
-    if 'currentCourseID' in request.session:
-        currentCourse = Courses.objects.get(pk=int(request.session['currentCourseID']))
-        context_dict['course_Name'] = currentCourse.courseName
-    else:
-        context_dict['course_Name'] = 'Not Selected'
-        context_dict['course_notselected'] = 'Please select a course'
-        currentCourse = 1
+    students = process_file(upfile.uploadedFile.name)
     
-    studentslistFile = request.FILES['studentslist']
-    studentslistFileName = studentslistFile.name   
-    filePath = '../oneUpER/media/textFiles/'+ studentslistFileName
-    students = process_file(filePath)
     for studentData in students:
-        uname = studentData[3]
-        
+
+        uname = studentData[3]       
         email = studentData[2]
-        pword = uname +'7' # has to be change by the instructor or the student
-        user = User.objects.create_user(uname, email, pword)
-        user.first_name = studentData[0]
-        user.last_name = studentData[1]
-        user.save()
+        pword = studentData[4]  # Student Banner/University ID
         
-        student = Student()
-        student.user = user
-        student.universityID = email
-        student.save()
+        # Check if student is in the system already
+        users = User.objects.filter(email = email)
+        if users:
+            #the student is in the system, get it
+            user = users[0] 
+            student = Student.objects.get(user = user)              
+                       
+        else:
+        # the student is not in the system, create a user/student       
+            user = User.objects.create_user(uname, email, pword)
+            user.first_name = studentData[0]
+            user.last_name = studentData[1]
+            user.save()
+            
+            student = Student()
+            student.user = user
+            student.universityID = email
+            student.save()
         
+        # register the student for this course
         studentRegisteredCourses = StudentRegisteredCourses()
         studentRegisteredCourses.studentID = student
         studentRegisteredCourses.courseID = currentCourse
         studentRegisteredCourses.avatarImage = anonymous_avatar
+        if ccparams.virtualCurrencyAdded:
+            studentRegisteredCourses.virtualCurrencyAmount += int(ccparams.virtualCurrencyAdded)
         studentRegisteredCourses.save()
+        
+        logger.debug('[POST] Created New Student With VC Amount: ' + str(studentRegisteredCourses.virtualCurrencyAmount))
+
+        # Create new Config Parameters
+        scparams = StudentConfigParams()
+        scparams.courseID = currentCourse
+        scparams.studentID = student
+        scparams.save()
         
     return redirect('createStudentListView')
             
