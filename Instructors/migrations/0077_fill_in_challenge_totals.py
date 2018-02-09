fa9afef764417d00f80b19aff99089e1612b59b4
Keith Irwin <irwinke@wssu.edu>
2018-02-09 15:41:51 -0500

    Added migration to fill in total scores.

---
+++ Instructors/migrations/0077_fill_in_challenge_totals.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+def fill_in_challenge_totals(apps,schema_editor):
+    Challenges = apps.get_model("Instructors","Challenges")
+    ChallengesQuestions = apps.get_model("Instructors","ChallengesQuestions")
+    for chall in Challenges.objects.all():
+        challQuests = ChallengesQuestions.objects.filter(challengeID = chall)
+        total = 0
+        for cq in challQuests:
+            total += cq.points
+        chall.totalScore = total
+
+class Migration(migrations.Migration):
+    
+    dependencies = [
+        ('Instructors', '0076_auto_20180209_1514'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_in_challenge_totals),
+    ]
