cf459e00b496e030caf4317385a4965feea11646
omar <omar@oneup.wssu.edu>
2018-03-13 11:25:52 -0400

    fixed bug with disabled buttons

--- Students/views/challengeDescriptionView.py
+++ Students/views/challengeDescriptionView.py
@@ -36,8 +36,9 @@ def ChallengeDescription(request):
             if request.GET['challengeID']:
                 #studentId = 1; # for now student id is 1 as there is no login table.. else studentd id will be the login ID that we get from the cookie or session
                 
-                if 'isWarmup' in request.session:
+                if 'isWarmup' in request.GET:
                     context_dict['isWarmup'] = request.GET['isWarmup']
+                   
                 else:
                     context_dict['isWarmup'] = False
                     
