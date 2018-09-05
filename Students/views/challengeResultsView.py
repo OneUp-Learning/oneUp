14ad5f03649ea952b05cde5e000131b0d6060ba7
Darina Dicheva <dichevad@wssu.edu>
2018-09-05 16:47:45 -0400

    Change for removing the distractors from the parson's problems in the student view

--- Students/views/challengeResultsView.py
+++ Students/views/challengeResultsView.py
@@ -286,6 +286,7 @@ def ChallengeResults(request):
                             lengthOfModelSolution = len(answer)
                             for index, line in enumerate(answer):
                                 line = re.sub("☃", "", line)
+                                line = re.sub(".*#distractor", "", line)
                                 line = re.sub("^[ ]{" + str(leadingSpacesCount) + "}", "", line)
                                 if index < len(answer)- 1:
                                     line = line +"\n"
@@ -293,9 +294,7 @@ def ChallengeResults(request):
                             
                             answer = ""
                             answer = answer.join(tabedanswer)
-                            
-                            
-                            
+                                                        
                             question['model_solution'] = answer
                             
                             #get all the data from the webpage
