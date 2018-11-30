2b294746678bc25282c0da7f9b120118288fd2bf
Gerardo Gonzalez <ggonzalezmoctezum117@rams.wssu.edu>
2018-11-30 14:56:29 -0500

    decorators fix for bug decorators.py

--- oneUp/decorators.py
+++ oneUp/decorators.py
@@ -3,13 +3,7 @@ from Instructors.models import Instructors
 
 # Used to check if the user is a Instructor if not takes the student to the Student home Page
 def instructorsCheck(user):
-    #instruct = Instructors.objects.filter(user=user)
     return user.groups.filter(name='Teachers').exists()
-    # if instruct:
-    #     print("We are a teacher")
-    #     return True
-
-    # return False
     
 #from django.contrib.auth.decorators import login_required, user_passes_test
 #from oneUp.decorators import instructorsCheck     
