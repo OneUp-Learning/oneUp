1332d8e74677f5b5dc163a9444959c20d545afec
James Cherry <jcherry106@rams.wssu.edu>
2019-03-18 09:56:57 -0400

    Changed goal end date format, added enum to dict function for goal type.

--- Students/views/goalsListView.py
+++ Students/views/goalsListView.py
@@ -11,13 +11,14 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from Instructors.models import Announcements, Instructors, Courses, Goals
 from Instructors.views.utils import utcDate, initialContextDict
 from Instructors.constants import default_time_str
-from datetime import datetime
+from datetime import datetime, date, time, timedelta
 from oneUp.decorators import instructorsCheck   
 from Students.models import StudentGoalSetting
 from Students.views.utils import studentInitialContextDict
 from Students.views.allAnnouncementsView import createContextForAnnouncementList
 
 from Students.models import StudentGoalSetting
+from Badges.enums import Goal
 
 
 # Added boolean to check if viewing from announcements page or course home page
@@ -25,54 +26,58 @@ def createContextForGoalsList(currentCourse, context_dict, courseHome):
 
     studentGoal_ID = []      
     student_ID = []
+    course_ID = []
     start_date = []
     end_date = []
     goal_Type = []
     targeted_Number = []
         
-    goals = StudentGoalSetting.objects.order_by('-timestamp')
+    goals = StudentGoalSetting.objects.filter(studentID=context_dict['student'],courseID=currentCourse).order_by('-timestamp')
     index = 0
     if not courseHome: # Shows all the announcements
         for goal in goals:
-            studentGoal_ID.append(goals.studentGoalID) #pk
-            student_ID.append(goals.studentID)
-            start_date.append(goals.timestamp)
+            studentGoal_ID.append(goal.studentGoalID) #pk
+            student_ID.append(goal.studentID)
+            start_date.append(goal.timestamp.strftime('%m/%d/%y'))
+            course_ID.append(goal.courseID)
             # if default end date (= unlimited) is stored, we don't want to display it on the webpage                   
-            endDate = goals.timestamp + datetime.timedelta(days=7)
-            end_date.append(endDate)
-            
-            #end_date calculation function here
-            
-            goal_Type.append(goals.goalType)
-            targeted_Number.append(goals.targetedNumber)
+            endDate = goal.timestamp + timedelta(days=7)
+            end_date.append(endDate.strftime('%m/%d/%y'))            
+            #end_date calculation function here            
+            goal_Type.append(goalTypeToString(goal.goalType))
+            targeted_Number.append(goal.targetedNumber)
     else: # Only shows the first three
         for goal in goals:
             if index < 1:
-                studentGoal_ID.append(goals.studentGoalID) #pk
-                student_ID.append(goals.studentID)
-                start_date.append(goals.timestamp)
-                end_date.append(goals.timestamp)
-                goal_Type.append(goals.goalType)
-                targeted_Number.append(goals.targetedNumber)
+                studentGoal_ID.append(goal.studentGoalID) #pk
+                student_ID.append(goal.studentID)
+                course_ID.append(goal.courseID)
+                start_date.append(goal.timestamp.strftime('%m/%d/%y'))
+                endDate = goal.timestamp + timedelta(days=7)
+                end_date.append(endDate.strftime('%m/%d/%y'))
+                goal_Type.append(goalTypeToString(goal.goalType))
+                targeted_Number.append(goal.targetedNumber)
                 index += 1
     
       
     # The range part is the index numbers.
-    context_dict['goal_range'] = zip(range(1,goals.count()+1),studentGoal_ID,student_ID,start_date,end_date,goal_Type,targeted_Number)
+    context_dict['goal_range'] = zip(range(1,goals.count()+1),studentGoal_ID,student_ID,course_ID,start_date,end_date,goal_Type,targeted_Number)
     return context_dict
 
     
 @login_required
 def goalsList(request):
 
-    context_dict, currentCourse = initialContextDict(request)
+    context_dict, currentCourse = studentInitialContextDict(request)
 
     context_dict = createContextForGoalsList(currentCourse, context_dict, False)
     
     return render(request,'Students/GoalsList.html', context_dict)
 
-
-
-            
+def goalTypeToString(gt):
+    genums = Goal.goals
+    #gname = 'Blank'    
+    
+    return genums[gt].get('displayName')
     
     
