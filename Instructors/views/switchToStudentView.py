68a8a5934cd28974ea5785a5053eef325729072a
Mahamat Oumar <omar.mho20@gmail.com>
2019-09-02 17:51:07 -0400

    Switching to student view will create a test student if there is none before switching

--- Instructors/views/switchToStudentView.py
+++ Instructors/views/switchToStudentView.py
@@ -6,25 +6,51 @@ Created on Sep 2, 2018
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
-from oneUp.decorators import instructorsCheck   
-  
+from oneUp.decorators import instructorsCheck
+from Students.models import Student, StudentRegisteredCourses, StudentConfigParams
+from Instructors.models import InstructorRegisteredCourses
+from Instructors.constants import anonymous_avatar
+
 
 @login_required
-@user_passes_test(instructorsCheck,login_url='/oneUp/students/StudentHome',redirect_field_name='')   
+@user_passes_test(instructorsCheck, login_url='/oneUp/students/StudentHome', redirect_field_name='')
 def switchToStudentView(request):
-    context_dict = { }
-    
-    context_dict["logged_in"]=request.user.is_authenticated
+    context_dict = {}
+
+    context_dict["logged_in"] = request.user.is_authenticated
     if request.user.is_authenticated:
         user = request.user
-        context_dict["username"]=user.username
-        
+        context_dict["username"] = user.username
+
+        if not Student.objects.filter(user=user):
+
+            student = Student()
+            student.user = user
+            student.universityID = user.email
+            student.isTestStudent = True
+            student.save()
+
+            ircs = InstructorRegisteredCourses.objects.filter(
+                instructorID=user)
+
+            for course in ircs:
+                student = Student.objects.get(user=user)
+                studentRegisteredCourses = StudentRegisteredCourses()
+                studentRegisteredCourses.studentID = student
+                studentRegisteredCourses.courseID = course.courseID
+                studentRegisteredCourses.avatarImage = anonymous_avatar
+                studentRegisteredCourses.save()
+
+                # Configure params for test student
+                scparams = StudentConfigParams()
+                scparams.courseID = course.courseID
+                scparams.studentID = student
+                scparams.save()
+
         if 'teacher' in request.GET:
             if 'courseID' in request.GET:
                 return redirect('/oneUp/students/StudentCourseHome')
             else:
                 return redirect('/oneUp/students/StudentHome')
-            
+
     return render(request, 'oneUp/home.html', context_dict)
-        
-        
--- Students/views/switchToInstructorView.py
+++ Students/views/switchToInstructorView.py
@@ -7,15 +7,16 @@ Created on Sep 2, 2018
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def switchToInstructorView(request):
-    context_dict = { }
-    
-    context_dict["logged_in"]=request.user.is_authenticated
+    context_dict = {}
+
+    context_dict["logged_in"] = request.user.is_authenticated
     if request.user.is_authenticated:
         user = request.user
-        context_dict["username"]=user.username
-        
+        context_dict["username"] = user.username
+
         if 'student' in request.GET:
             if 'courseId' in request.GET:
                 return redirect('/oneUp/instructors/instructorCourseHome')
@@ -23,5 +24,3 @@ def switchToInstructorView(request):
                 return redirect('/oneUp/instructors/instructorHome')
 
     return render(request, '/oneUp/home.html', context_dict)
-        
-        
