c506aeb2580801e67ea4221a75030d84de019982
Gerardo Gonzalez <ggonzalezmoctezum117@rams.wssu.edu>
2019-09-27 16:12:52 -0400

    In student assign points for activities the students are ordered by last name

--- Instructors/views/activityAssignPointsView.py
+++ Instructors/views/activityAssignPointsView.py
@@ -114,6 +114,8 @@ def activityAssignPointsView(request):
 def createContextForPointsAssignment(request, context_dict, currentCourse):
     student_ID = []
     student_Name = []
+    student_first_name = []
+    student_last_name = []
     student_Points = [] 
     student_Bonus = []   
     student_Feedback = []
@@ -125,9 +127,11 @@ def createContextForPointsAssignment(request, context_dict, currentCourse):
         student = stud_course.studentID
         student_ID.append(student.id)
         if student.isTestStudent:
-            student_Name.append("Test Student")
+            student_first_name.append("Test")
+            student_last_name.append("Student")
         else:
-            student_Name.append((student).user.get_full_name())
+            student_first_name.append((student).user.first_name)
+            student_last_name.append((student).user.last_name)
         
         #zipFile_Name.append(student.user.first_name + student.user.last_name + Activities.objects.get(activityID = request.GET['activityID']).activityName + '.zip')
         
@@ -168,10 +172,10 @@ def createContextForPointsAssignment(request, context_dict, currentCourse):
         
     context_dict['activityID'] = request.GET['activityID']
     context_dict['activityName'] = Activities.objects.get(activityID = request.GET['activityID']).activityName
-
-    student_list = sorted(list(zip(range(1,len(student_ID)+1),student_ID,student_Name,student_Points, student_Bonus, student_Feedback, File_Name)), key=lambda tup:tup[2])
+    print(student_first_name, student_first_name)
+    student_list = sorted(list(zip(range(1,len(student_ID)+1),student_ID,student_first_name, student_last_name,student_Points, student_Bonus, student_Feedback, File_Name)), key=lambda tup:tup[3])
     ##we have to find the index for the test student and remove them from the sorted list
-    test_index = [y[2] for y in student_list].index('Test Student')
+    test_index = [y[2] for y in student_list].index('Test')
     test_student_ob = student_list[test_index]
     del student_list[test_index]
 
