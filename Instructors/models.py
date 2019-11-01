68ecbade63e8fd0855bdff9f799d83739aa012c1
Mahamat Oumar <omar.mho20@gmail.com>
2019-11-01 14:48:24 -0400

    added capability to upload program files to dynamic questins

--- Instructors/models.py
+++ Instructors/models.py
@@ -78,7 +78,15 @@ class Questions(models.Model):
 #     courseID = models.ForeignKey(Courses, verbose_name="the related course", db_index=True)
     def __str__(self):              
         return str(self.questionID)+","+self.preview
-    
+
+class QuestionProgrammingFiles(models.Model):
+    programmingFileID = models.AutoField(primary_key=True)
+    questionID = models.ForeignKey(Questions, on_delete=models.CASCADE, null=True, verbose_name= 'the related question')
+    programmingFileName = models.CharField(max_length=200)
+    programmingFileFolderName = models.CharField(max_length=200)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    programmingFileUploader = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name="Creator", db_index=True)
+     
 class StaticQuestions(Questions):
     questionText = models.CharField(max_length=10000)
     correctAnswerFeedback = models.CharField(max_length=1000, default="")
