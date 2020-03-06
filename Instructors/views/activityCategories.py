aee12c0ef9799307ce751bde8833937a7df8ea83
Mahamat Oumar <omar.mho20@gmail.com>
2020-03-06 14:26:16 -0500

    added drag and drop to reorder for serious challs and topics, categories, and activities

--- Instructors/views/activityCategories.py
+++ Instructors/views/activityCategories.py
@@ -14,54 +14,68 @@ from django.template.defaultfilters import default
 from django.template.context_processors import request
 from oneUp.decorators import instructorsCheck
 
+
 @login_required
-@user_passes_test(instructorsCheck,login_url='/oneUp/students/StudentHome',redirect_field_name='')  
+@user_passes_test(instructorsCheck, login_url='/oneUp/students/StudentHome', redirect_field_name='')
 def activityCatList(request):
-    context_dict, currentCourse = initialContextDict(request)   
-    categories = ActivitiesCategory.objects.filter(courseID=currentCourse).exclude(name=uncategorized_activity)
+    context_dict, currentCourse = initialContextDict(request)
+    categories = ActivitiesCategory.objects.filter(
+        courseID=currentCourse).exclude(name=uncategorized_activity).order_by("catPosition")
+    print("cats")
+    print(categories)
     context_dict['cats'] = categories
 
-    return render(request,'Instructors/ActivityCategories.html', context_dict)
+    return render(request, 'Instructors/ActivityCategories.html', context_dict)
+
 
 @login_required
-@user_passes_test(instructorsCheck,login_url='/oneUp/students/StudentHome',redirect_field_name='')  
+@user_passes_test(instructorsCheck, login_url='/oneUp/students/StudentHome', redirect_field_name='')
 def activityCatCreate(request):
     context_dict, currentCourse = initialContextDict(request)
-    
+
     if request.POST:
         # Check the category exists and has to be edited
         if 'catID'in request.POST:
-            currentCat = ActivitiesCategory.objects.get(categoryID=request.POST['catID'])
-        else:           
+            currentCat = ActivitiesCategory.objects.get(
+                categoryID=int(request.POST['catID']))
+        else:
             currentCat = ActivitiesCategory()
-            
+
         currentCat.name = request.POST['catName']
         currentCat.courseID = currentCourse
         currentCat.xpWeight = request.POST['xpWeight']
         currentCat.save()
-        
+
+        if 'activityList' in request.POST:
+            return redirect('/oneUp/instructors/activitiesList')
+
         return redirect('/oneUp/instructors/activityCats')
-    
+
     if 'catID' in request.GET:
         catID = request.GET['catID']
-        cat = ActivitiesCategory.objects.filter(courseID=currentCourse, pk=catID).first()
-        
+        cat = ActivitiesCategory.objects.filter(
+            courseID=currentCourse, pk=catID).first()
+
         context_dict['catID'] = catID
         context_dict['catName'] = cat.name
         context_dict['xpWeight'] = cat.xpWeight
     else:
-        context_dict['acts'] = Activities.objects.filter(courseID=currentCourse)
+        context_dict['acts'] = Activities.objects.filter(
+            courseID=currentCourse)
         context_dict['xpWeight'] = 1
-        
+
     return render(request, 'Instructors/ActivityCategoryCreateForm.html', context_dict)
 
+
 @login_required
-@user_passes_test(instructorsCheck,login_url='/oneUp/students/StudentHome',redirect_field_name='')  
+@user_passes_test(instructorsCheck, login_url='/oneUp/students/StudentHome', redirect_field_name='')
 def activityCatDelete(request):
     if request.POST:
-        cat = ActivitiesCategory.objects.filter(pk=request.POST['catID']).first()
+        cat = ActivitiesCategory.objects.filter(
+            pk=request.POST['catID']).first()
         if cat is not None:
-            defaultCat = ActivitiesCategory.objects.filter(name=uncategorized_activity, courseID=cat.courseID)
+            defaultCat = ActivitiesCategory.objects.filter(
+                name=uncategorized_activity, courseID=cat.courseID)
             if not defaultCat.exists():
                 defaultCat = ActivitiesCategory()
                 defaultCat.name = uncategorized_activity
@@ -77,5 +91,3 @@ def activityCatDelete(request):
             cat.delete()
 
     return redirect('/oneUp/instructors/activityCats')
-    
-    
--- Instructors/views/reorderViews.py
+++ Instructors/views/reorderViews.py
@@ -5,7 +5,7 @@ Created on February 27, 2020
 '''
 
 from django.http import JsonResponse
-from Instructors.models import CoursesTopics, Challenges
+from Instructors.models import CoursesTopics, Challenges, Activities, ActivitiesCategory
 import json
 from Instructors.views import utils
 
@@ -38,7 +38,7 @@ def reorderTopics(request):
 
 
 def reorderChalls(request):
-    ''' This view is called by an ajax function to reorder topics'''
+    ''' This view is called by an ajax function to reorder challenges'''
 
     context_dict, currentCourse = utils.initialContextDict(request)
 
@@ -52,6 +52,50 @@ def reorderChalls(request):
                     challengeID=int(idPos[0]), courseID=currentCourse)
                 chall.challengePosition = int(idPos[1])
                 chall.save()
+            except:
+                continue
+
+        return JsonResponse({"success": True})
+    return JsonResponse({"success": False})
+
+
+def reorderCategories(request):
+    ''' This view is called by an ajax function to reorder categories'''
+
+    context_dict, currentCourse = utils.initialContextDict(request)
+
+    if request.POST:
+        catIdsPositions = request.POST.getlist("catIdsPositions[]")
+        for catdPos in catIdsPositions:
+            idPos = catdPos.split("---")
+
+            try:
+                cat = ActivitiesCategory.objects.get(
+                    categoryID=int(idPos[0]), courseID=currentCourse)
+                cat.catPosition = int(idPos[1])
+                cat.save()
+            except:
+                continue
+
+        return JsonResponse({"success": True})
+    return JsonResponse({"success": False})
+
+
+def reorderActivities(request):
+    ''' This view is called by an ajax function to reorder activities'''
+
+    context_dict, currentCourse = utils.initialContextDict(request)
+
+    if request.POST:
+        activityIdsPositions = request.POST.getlist("activityIdsPositions[]")
+        for activityIdPos in activityIdsPositions:
+            idPos = activityIdPos.split("---")
+
+            try:
+                activity = Activities.objects.get(
+                    activityID=int(idPos[0]), courseID=currentCourse)
+                activity.activityPosition = int(idPos[1])
+                activity.save()
             except:
                 continue
 
