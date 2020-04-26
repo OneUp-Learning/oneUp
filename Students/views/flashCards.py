4205cd983495968c90967dae713f08dd07160bb3
Gerardo Gonzalez <ggonzalezmoctezum117@rams.wssu.edu>
2020-04-26 00:12:05 -0400

    flash cards select multiple, select all

--- Students/views/flashCards.py
+++ Students/views/flashCards.py
@@ -1,5 +1,5 @@
 '''
-Created on April 21, 2020
+Created on April 20, 2020
 
 @author: GGM
 '''
@@ -9,7 +9,7 @@ from django.http import JsonResponse
 from Students.views.utils import studentInitialContextDict, str2bool
 from Students.models import studentFlashCards, Student
 from Instructors.models import FlashCardGroupCourse, FlashCardGroup, FlashCardToGroup, FlashCards
-
+from Instructors.views.utils import datetime_to_local
 from oneUp.decorators import instructorsCheck
 import datetime
 
@@ -44,7 +44,7 @@ def flashCardUsed(request):
                 studentFlashCard.studyDate = getOptimumStudyTime(studentFlashCard.cardBin)
             if(not gotIt):
                 studentFlashCard.cardBin = 0
-                studentFlashCard.studyDate = datetime.datetime.now()
+                studentFlashCard.studyDate = datetime_to_local(datetime.datetime.now())
             studentFlashCard.timesSeen = studentFlashCard.timesSeen + 1
     
             studentFlashCard.save()
@@ -58,14 +58,36 @@ def flashCards(request):
     context_dict, current_course = studentInitialContextDict(request)
     if 'groupID' in request.GET:
         groupID = request.GET['groupID']
-        context_dict['flash_range'] = getFlashCardsForPractice(groupID, request)
-        context_dict['groupID'] = groupID
+        if groupID == "ALL":
+            context_dict['flash_range'] = getFlashCards(groupID, request, True)
+        else:
+            context_dict['flash_range'] = getFlashCards(groupID, request)
+    if 'study-checkboxes' in request.POST:
+        listOfRequestedCardsGroups = []
+        requested_groups = request.POST.getlist('study-checkboxes')
+
+        #maybe the student selected one, just pop(cm) it 
+        if(len(requested_groups) == 1):
+            requested_groups = requested_groups.pop()
+        context_dict['flash_range'] = getFlashCards(requested_groups, request)
     return render(request,'Students/FlashCards.html',context_dict)
 
 def flashCardsList(request):
     context_dict, current_course = studentInitialContextDict(request)
     context_dict['group_range'] = createGroupRange(current_course)
     return render(request,'Students/FlashCardsList.html',context_dict)
+
+def getOptimumStudyTime(time_bin):
+    #two days later, a week, three weeks later.
+    current_time = datetime_to_local(datetime.datetime.now())
+    if(time_bin == 1):
+        delta = datetime.timedelta(days=2)
+    if(time_bin == 2):
+        delta = datetime.timedelta(days=7)
+    if(time_bin == 3):
+        delta = datetime.timedelta(days=21)
+    future_time = current_time + delta
+    return future_time
 
 def createGroupRange(current_course):
     card_groups = FlashCardGroupCourse.objects.filter(courseID=current_course).order_by("groupPos")
@@ -76,6 +98,44 @@ def createGroupRange(current_course):
         group = FlashCardGroup.objects.get(groupID=groupID)
         groupList.append({'groupID': groupID, 'groupName': group.groupName , "cardCount": cardCount})
     return groupList
+
+def getFlashCardObjFromID(flashID):
+    return FlashCards.objects.get(flashID=flashID)
+
+def getFlashCards(groupIDs, request, all_cards=False):
+    #get all the cards in the course no exceptions
+    if(all_cards):        
+        context_dict, currentCourse = studentInitialContextDict(request)
+        card_groups = FlashCardGroupCourse.objects.filter(courseID=currentCourse).exclude(groupID_id__groupName="Unassigned")
+
+        groupIDs = []
+        for card_group in card_groups:
+            groupIDs.extend(list(FlashCardToGroup.objects.filter(groupID=card_group.groupID)))
+        return getCardsFromGroup(groupIDs,request, True)
+    
+    #get the the flash cards for a predetermined list of groups
+    if(len(groupIDs)> 1):
+        card_groups = []
+        for groupID in groupIDs:
+            card_groups.extend(list(FlashCardToGroup.objects.filter(groupID=groupID)))
+        return getCardsFromGroup(card_groups, request)
+    
+    #singular card group
+    if(len(groupIDs) == 1):
+        card_group = list(FlashCardToGroup.objects.filter(groupID=groupIDs))
+        return getCardsFromGroup(card_group, request)
+
+def getCardsFromGroup(flashCards, request, all_cards=False):
+    flash_cards = []
+    for flashCardToGroup in flashCards:
+        flash_card = flashCardToGroup.flashID
+        groupID = flashCardToGroup.groupID.groupID
+
+        #all cards is used to overide the filtering for the case of all cards
+        if(cardShouldBeAdded(flash_card.flashID, request) or all_cards):
+            flash_cards.append({'question': flash_card.front, 'answer': flash_card.back,
+        'flashName': flash_card.flashName, 'flashID': flash_card.flashID, 'groupID': groupID})
+    return flash_cards
 
 def cardShouldBeAdded(flashID, request):
     student = Student.objects.get(user=request.user)
@@ -93,31 +153,6 @@ def cardShouldBeAdded(flashID, request):
             return True
 
         #if they have one and its date is due or behind us add it
-        #if(studentFlashCard.studyDate <= datetime.datetime.now()):
-        #    return True
-
-def getFlashCardsForPractice(groupID, request):
-    flash_cards_to_group = FlashCardToGroup.objects.filter(groupID=groupID)
-    flash_cards = []
-    for flash_card_to_group in flash_cards_to_group:
-        flashCardID = flash_card_to_group.flashID.flashID
-        flashCard = flash_card_to_group.flashID
-        if(cardShouldBeAdded(flashCardID, request)):
-            flash_cards.append({'question': flashCard.front, 'answer': flashCard.back,
-        'flashName': flashCard.flashName, 'flashID': flashCardID})
-    return flash_cards
-
-def getFlashCardObjFromID(flashID):
-    return FlashCards.objects.get(flashID=flashID)
-
-def getOptimumStudyTime(time_bin):
-    #two days later, a week, three weeks later.
-    current_time = datetime.datetime.now() 
-    if(time_bin == 1):
-        delta = datetime.timedelta(days=2)
-    if(time_bin == 2):
-        delta = datetime.timedelta(days=7)
-    if(time_bin == 3):
-        delta = datetime.timedelta(days=21)
-    future_time = current_time + delta
-    return future_time
+        if(datetime_to_local(studentFlashCard.studyDate) <= datetime_to_local(datetime.datetime.now())):
+           return True
+    return False
