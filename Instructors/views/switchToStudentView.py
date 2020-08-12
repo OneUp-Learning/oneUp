726d95eddaa9036ea7ee7c2a84f517ec5845d62c
Keith Irwin <irwinke@wssu.edu>
2020-08-12 17:06:06 -0400

    Bugfix for test student stuff with SVC entries.

--- Instructors/views/switchToStudentView.py
+++ Instructors/views/switchToStudentView.py
@@ -40,11 +40,11 @@ def switchToStudentView(request):
                 studentRegisteredCourses.studentID = student
                 studentRegisteredCourses.courseID = course.courseID
                 studentRegisteredCourses.avatarImage = anonymous_avatar
-                ccparams = CourseConfigParams.objects.get(courseID = course)
+                ccparams = CourseConfigParams.objects.get(courseID = course.courseID)
                 if ccparams.virtualCurrencyAdded:
                     # We have now switched to the canonical virtual currency amount a student has being determined by their transactions,
                     # so we first add a StudentVirtualCurrency entry to show their gain and then we adjust the virtualCurrencyAmount.
-                    createSCVforInstructorGrant(student,course,ccparams.virtualCurrencyAdded)
+                    createSCVforInstructorGrant(student,course.courseID,ccparams.virtualCurrencyAdded)
                     studentRegisteredCourses.virtualCurrencyAmount += int(ccparams.virtualCurrencyAdded)
                 studentRegisteredCourses.save()
 
