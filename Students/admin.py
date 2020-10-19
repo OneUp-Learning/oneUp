26ed2399f09c54af7c94a6e980f5e77110b679b3
cmickle <cmickle@pop-os.localdomain>
2020-10-19 17:51:20 -0400

    Added models for teams
    added in team models
    updated CCParams and Challenge models with new fields for teams

--- Students/admin.py
+++ Students/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from Students.models import Student, StudentRegisteredCourses, StudentChallenges, StudentChallengeQuestions, StudentBadges, DuelChallenges, Winners, StudentGoalSetting
 from Students.models import StudentChallengeAnswers, MatchShuffledAnswers, StudentCourseSkills, StudentVirtualCurrency, StudentVirtualCurrencyRuleBased, StudentProgressiveUnlocking, StudentActions, StudentActionsLoop
 from Students.models import StudentEventLog,StudentConfigParams,StudentLeaderboardHistory, StudentActivities, StudentFile, StudentVirtualCurrencyTransactions, UploadedAvatarImage, StudentAttendance, StudentStreaks, PeriodicallyUpdatedleaderboards, Callouts
-
+from Students.models import TeamActivities, TeamChallengeQuestions, TeamChallenges, Teams, TeamStudents
 admin.site.register(Student)
 admin.site.register(StudentActivities)
 admin.site.register(StudentChallenges)
@@ -32,3 +32,8 @@ admin.site.register(StudentProgressiveUnlocking)
 admin.site.register(StudentActions)
 admin.site.register(StudentActionsLoop)
 admin.site.register(StudentGoalSetting)
+admin.site.register(Teams)
+admin.site.register(TeamStudents)
+admin.site.register(TeamChallenges)
+admin.site.register(TeamChallengeQuestions)
+admin.site.register(TeamActivities)
