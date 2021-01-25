d172d548a56eef8ad3ebb99f74f53597ff59b534
Keith Irwin <irwinke@wssu.edu>
2021-01-25 01:47:34 -0500

    Checkpoint commit for adding skill variables.

--- Badges/enums.py
+++ Badges/enums.py
@@ -684,6 +684,7 @@ class ObjectTypes():
     activityCategory=1307
     virtualCurrencySpendRule=1308 # Used for manual virtual currency rules/transactions
     flashcard = 1309
+    skill = 1310
     objectTypes = {
         challenge:"challenge",
         activity:"activity",
@@ -693,7 +694,8 @@ class ObjectTypes():
         topic:"topic",
         activityCategory:"activityCategory",
         virtualCurrencySpendRule:"virtualCurrencySpendRule",
-        flashcard:"flashcard"
+        flashcard:"flashcard",
+        skill:"skill",
     }
 
 class OperandTypes():
